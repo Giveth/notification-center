69ef324870bc15bdf657a32f3bbaafb5c7fb4ef4
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-08-29 12:06:12 +0430

    Add cors to express

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { DataSource } from 'typeorm';
 import bodyParser from 'body-parser';
 import { errorHandler } from './middlewares/errorHandler';
 import {logger} from "./utils/logger";
+const cors = require('cors');
 
 export let dbConnection: DataSource;
 export const initDbConnection = async () => {
@@ -22,6 +23,39 @@ export const initServer = async () => {
 
   // This is for serve swagger
   app.use(express.static('public'));
+
+  const whitelistHostnames: string[] = (
+      process.env.HOSTNAME_WHITELIST as string
+  ).split(',');
+
+  const corsOptions = {
+    origin(origin :string, callback:(a:Error | null, b ?: any)=>{}) {
+      if (!origin) {
+        // allow requests with no origin (like mobile apps, Curl, ...)
+        return callback(null, true);
+      }
+
+      // removing http:// , https://, and :port
+      const formattedOrigin = origin
+          .replace('https://', '')
+          .replace('http://', '')
+          .split(':')[0];
+
+      for (const allowedOrigin of whitelistHostnames) {
+        // passing all subdomains of whitelist hosts, for instance x.vercel.app, x.giveth.io,...
+        if (
+            formattedOrigin === allowedOrigin ||
+            formattedOrigin.endsWith(`.${allowedOrigin}`)
+        ) {
+          return callback(null, true);
+        }
+      }
+
+      logger.error('CORS error', { whitelistHostnames, origin });
+      callback(new Error('Not allowed by CORS ' + origin));
+    },
+  };
+  app.use(cors(corsOptions));
 
   app.use(bodyParser.json());
   app.use(
