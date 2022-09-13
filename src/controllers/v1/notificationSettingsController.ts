e329e09612641e42d5f98c43f625a7528b3a6362
Carlos Quintero <cquinterom096@outlook.com>
2022-09-12 23:59:31 -0500

    fix swagger annotation

--- src/controllers/v1/notificationSettingsController.ts
+++ src/controllers/v1/notificationSettingsController.ts
@@ -10,7 +10,7 @@ import { UserAddress } from '../../entities/userAddress';
 @Tags('NotificationSettings')
 export class NotificationSettingsController {
   @Put('/')
-  @Security('basicAuth')
+  @Security('JWT')
   public async updateNotificationSetting(
     @Body()
     body: {
