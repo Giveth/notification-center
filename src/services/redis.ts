7b21421e36f91811c7c1d728ceb53115f901e102
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2022-11-10 11:57:57 +0300

    Fix redis config

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -2,7 +2,7 @@ import { RedisOptions } from 'ioredis';
 // tslint:disable-next-line:no-var-requires
 const Redis = require('ioredis');
 export const redisConfig: RedisOptions = {
-  port: Number(process.env.REDIS_HOST) || 6379,
+  port: Number(process.env.REDIS_PORT) || 6379,
   host: process.env.REDIS_HOST || 'localhost',
 };
 if (process.env.REDIS_PASSWORD) {
