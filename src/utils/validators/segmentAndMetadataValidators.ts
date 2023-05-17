4abc1ef1962eea5aeb6489e1300112c351845b53
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-05-17 16:38:16 +0330

    Make email nullable in input values

--- src/utils/validators/segmentAndMetadataValidators.ts
+++ src/utils/validators/segmentAndMetadataValidators.ts
@@ -19,7 +19,7 @@ export const validateWithJoiSchema = (data: any, schema: ObjectSchema) => {
 // Define all joi schemas here
 const projectRelatedTrackerSchema = Joi.object({
   // seems we have to different schemas, not good TODO CHANGE ON IMPACT GRAPH
-  email: Joi.string(),
+  email: Joi.string().allow(null).allow(''),
   title: Joi.string().required(),
   firstName: Joi.string().allow(null, ''),
   lastName: Joi.string().allow(null, ''),
@@ -58,7 +58,7 @@ const givPowerUnLockedSchema = Joi.object({
 });
 
 const donationTrackerSchema = Joi.object({
-  email: Joi.string().allow(null, ''), // if anonymous
+  email: Joi.string().allow(null).allow(''), // if anonymous
   title: Joi.string().required(),
   firstName: Joi.string().allow(null, ''),
   lastName: Joi.string().allow(null, ''),
