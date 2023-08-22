a29efe0ebe50605fbb19994817defc1a042f3aaf
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-08-22 14:11:09 +0330

    dont run changeNotificationCopies1692698983844 in test and development ENVs

--- migrations/1692698983844-changeNotificationCopies.ts
+++ migrations/1692698983844-changeNotificationCopies.ts
@@ -22,6 +22,16 @@ export class changeNotificationCopies1692698983844
   }
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    if (
+      process.env.NODE_ENV === 'test' ||
+      process.env.NODE_ENV === 'development'
+    ) {
+      // Running these migrations in test and development environments would make the tests fail
+      // because the notification types are not created in the test database
+      // In future we should use raw SQL queries to update the notification types
+      return;
+    }
+
     const notificationTypesArray = [
       {
         eventName: NOTIFICATION_TYPE_NAMES.PROJECT_LISTED_OWNER,
