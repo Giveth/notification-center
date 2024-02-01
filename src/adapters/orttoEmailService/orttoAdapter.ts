f11d2a92f07223e4178b09aa67f758aabee64b74
Ramin <raminramazanpour@gmail.com>
2024-02-01 14:18:46 +0330

    create ortto adapter

---
+++ src/adapters/orttoEmailService/orttoAdapter.ts
@@ -0,0 +1,23 @@
+import { logger } from '../../utils/logger';
+import axios from 'axios';
+
+export const orttoActivityCall = async (data: any) => {
+  try {
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: process.env.ORTTO_ACTIVITY_API,
+      headers: {
+        'X-Api-Key': process.env.ORTTO_API_KEY as string,
+        'Content-Type': 'application/json'
+      },
+      data
+    };
+    await axios.request(config);
+  } catch (e) {
+    logger.error('orttoActivityCall error', {
+      e,
+    });
+    throw e;
+  }
+}
