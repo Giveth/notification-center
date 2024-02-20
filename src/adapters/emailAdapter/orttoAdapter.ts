376fb4bfb859cacae09b287651c0634fa4a019b0
Ramin <raminramazanpour@gmail.com>
2024-02-20 17:49:24 +0330

    turn of Ortto calls on staging

--- src/adapters/emailAdapter/orttoAdapter.ts
+++ src/adapters/emailAdapter/orttoAdapter.ts
@@ -9,6 +9,7 @@ export class OrttoAdapter implements OrttoAdapterInterface{
       if (!data){
         throw new Error('callOrttoActivity input data is empty')
       }
+      if (process.env.NODE_ENV !== 'production') return;
       const config = {
         method: 'post',
         maxBodyLength: Infinity,
